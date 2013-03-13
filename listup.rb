b9c4a1676bc89fbdb482b1d7a6783c79edd0f3d6
toshiya-hirasawa <k4th66gg@gmail.com>
2013-03-14 00:04:41 +0900

    hash version

--- listup.rb
+++ listup.rb
@@ -5,8 +5,10 @@ item_price = ""
 file = open("wish.html")
 
 file.each_line do |line|
+	#商品名
 	if line =~ /small productTitle/
 		flag = "on" 
+		#改行に対応
 	elsif flag == "on" 
 		if line =~ /a href/
 			item_name = line.gsub(/<[^<>]*>/,"")
@@ -14,13 +16,16 @@ file.each_line do |line|
 		end
 	end
 
+	#価格
 	if line =~ /wlPriceBold/
 		item_price = line.gsub(/<[^<>]*>/,"").gsub(/.*¥/,"¥")
 	end
 	
+	#連想配列に追加
 	item[item_name] = item_price
 end
 
+#改行を削除して連想配列を表示
 item.each do |name,price|
 	puts name.delete("\n") + "\t" + price
 end
