cbf739d879041ff40dc05b210c0c576efc2009d1
toshiya-hirasawa <k4th66gg@gmail.com>
2013-03-14 00:46:21 +0900

    normal array

--- listup.rb
+++ listup.rb
@@ -1,32 +1,39 @@
 flag = "stop"
-item = Hash.new
-item_name = ""
-item_price = ""
 file = open("wish.html")
+
+item = []
 
 file.each_line do |line|
 	#商品名
 	if line =~ /small productTitle/
 		flag = "on" 
-		#改行に対応
+	#改行に対応
 	elsif flag == "on" 
 		if line =~ /a href/
 			item_name = line.gsub(/<[^<>]*>/,"")
 		flag = "stop" 
+		#配列に格納
+		item << item_name
 		end
 	end
 
 	#価格
 	if line =~ /wlPriceBold/
-		item_price = line.gsub(/<[^<>]*>/,"").gsub(/.*¥/,"¥")
+		line =~ /¥ [0-9,]*/
+		item_price = $&
+		#item_price = line.gsub(/<[^<>]*>/,"").gsub(/.*¥/,"¥")
+		#配列に格納
+		item << item_price
 	end
-	
-	#連想配列に追加
-	item[item_name] = item_price
+
+	#写真
+	if line =~ /Product Image/
+		line =~ /src="http.*jpg/
+		item_img = $&.to_s.sub(/src="/,"")
+		item << item_img
+	end
 end
 
-#改行を削除して連想配列を表示
-item.each do |name,price|
-	puts name.delete("\n") + "\t" + price
-end
+puts item
+
 file.close
